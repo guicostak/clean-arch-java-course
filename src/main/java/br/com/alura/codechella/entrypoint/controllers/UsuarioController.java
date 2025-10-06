e5bdcd841151df1c9b220a3c1dd80b760cdb9169
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2025-10-05 23:02:03 -0300

    feat: creation of User controller

--- src/main/java/br/com/alura/codechella/entrypoint/controllers/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/entrypoint/controllers/UsuarioController.java
@@ -1,5 +1,8 @@
 package br.com.alura.codechella.entrypoint.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
     
 }
